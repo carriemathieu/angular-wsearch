4a0cca5dfe775f989cc949c6c633fb1d6e47fe72
Carrie Mathieu <carrieburks@gmail.com>
2021-08-11 17:29:23 -0400

    recieve fetch results, display in table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pages: any = [];
+  // wikipedia will be added as a 'private' property automatically to the instance of App component
+  // wikipedia will be the type of: WikipediaService
+  constructor(private wikipedia: WikipediaService) {}
+
   onTerm(term: string){
-    
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search
+    })
   }
 }
--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+// angular automatically creates instance of wikipedia service & makes available to components
 export class WikipediaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  search(term: string) {
+    return this.http.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*'
+      }
+    });
+  }
 }
