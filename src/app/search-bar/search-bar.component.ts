2b56e90cf02d7b7b41faf45136c608c665db06f8
Carrie Mathieu <carrieburks@gmail.com>
2021-08-11 15:45:28 -0400

    add form submit event listener

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  term = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    
+  }
 }
