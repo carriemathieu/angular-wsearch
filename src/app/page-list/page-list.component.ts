359cdd4efaa8a65e0f88e5f990530a21f3e22872
Carrie Mathieu <carrieburks@gmail.com>
2021-08-12 10:23:10 -0400

    update styling

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PageListComponent implements OnInit {
   @Input() pages = [];
-
+  
   constructor() { }
 
   ngOnInit(): void {
